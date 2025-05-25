f5ee1fe1d101f3989a0b1fce08b260cb698a6c38
Simon Kadlec <simon.kadlec@outlook.de>
2025-05-25 16:05:52 +0200

    small changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const e = require("express");
+var cors = require('cors');
 
 var app = express();
 
