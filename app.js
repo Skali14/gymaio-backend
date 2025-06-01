fb76b141b0bf8f6cebe7f13e2f605cde6ccdd172
Simon Kadlec <simon.kadlec@outlook.de>
2025-06-01 23:31:14 +0200

    changed upload size limit to 50mb (necessary for exercise image upload)

--- app.js
+++ app.js
@@ -29,9 +29,10 @@ var app = express();
 
 app.use(cors()); // Enable CORS for all routes - good for development
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
+
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
